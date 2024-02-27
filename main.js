135871f6e3bcbbb54c90bf3ece4a390d5555df88
MatteoStrazzera <mattestrazz@gmail.com>
2024-02-27 15:58:41 +0100

    add function and conditions to check the userword input in first exercise in js

--- main.js
+++ main.js
@@ -20,8 +20,27 @@ poi le lettere reversate -> .reverse()
 e infine le lettere vengono riunite -> .join('')
 */
 
-const reverseWord = userWord.split('').reverse().join('');
+// const reverseWord = userWord.split('').reverse().join('');
 
 // console.log(reverseWord);
 
 // Creare una funzione per capire se la parola inserita è palindroma
+
+function isPalindrome(userWord) {
+    // console.log('works');
+    const reverseWord = userWord.split('').reverse().join('');
+    
+    // creiamo le condizioni per verificare se userWord è palindromo
+    if (userWord === reverseWord) {
+        
+        console.log(`La parola ${userWord} è palindroma`);
+
+    } else {
+
+        console.log(`La parola ${userWord} non è palindroma`);
+    }
+
+
+}
+
+isPalindrome(userWord);
