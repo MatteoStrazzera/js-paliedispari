044172129290af6c26b61b92c3f7db42403f035c
MatteoStrazzera <mattestrazz@gmail.com>
2024-02-27 15:37:52 +0100

    add variables for user input and to reverse the userWord in first exercise in js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('connesso');
+// console.log('connesso');
+
+/*
+
+Palidroma
+Chiedere all’utente di inserire una parola, Creare una funzione per capire se la parola inserita è palindroma
+
+*/
+
+// Chiedere all’utente di inserire una parola
+
+const userWord = prompt('Inserisci una parola per verificare che sia palindroma');
+
+// console.log(userWord);
+
+/*
+creiamo la variabile in cui la parola inserita dall'utente venga:
+scomposta in lettere singole -> .split('') 
+poi le lettere reversate -> .reverse() 
+e infine le lettere vengono riunite -> .join('')
+*/
+
+const reverseWord = userWord.split('').reverse().join('');
+
+// console.log(reverseWord);
+
+// Creare una funzione per capire se la parola inserita è palindroma
